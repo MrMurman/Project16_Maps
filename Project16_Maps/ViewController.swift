44dc1c0d99b2f3e09c9b9cc71b83d3db5b030dc0
MrMurman <74971676+MrMurman@users.noreply.github.com>
2021-08-03 13:54:48 +0300

    Initial Commit

---
+++ Project16_Maps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project16_Maps
+//
+//  Created by Андрей Бородкин on 03.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
